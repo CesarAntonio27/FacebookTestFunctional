56bcd50383958396693e8dac5a1ec067a1818811
CesarAntonio27 <65976830+CesarAntonio27@users.noreply.github.com>
2021-07-20 15:19:41 -0500

    codigo funcionando

--- src/test/java/com/titanium/automation/FacebookTest.java
+++ src/test/java/com/titanium/automation/FacebookTest.java
@@ -39,7 +39,7 @@ public class FacebookTest {
 	By logoutLocator = By.xpath("(//span[contains(text(),'Cerrar sesión')])");
 	By gmailLocator = By.xpath("(//input[@name='email'])[1]");
 	By passwordLocator = By.xpath("(//input[@name='pass'])[1]");
-	By loginLocator = By.xpath("(//label[@id='loginbutton'])[1]");
+	By loginLocator = By.xpath("(//button[@name='login'])[1]");
 	By inicioLocator = By.xpath("//a[@aria-label='Inicio']");
 	By seleccionarCuentaLocator = By.xpath("//input[@type='checkbox']");
 	By eliminarSeleccionLocator = By.xpath("(//div[@aria-label='Eliminar selección'])[2]");
@@ -58,30 +58,31 @@ public class FacebookTest {
 	public void createAccount() throws InterruptedException {
 		driver.manage().window().maximize();
 		//accede a la pagina 
-		driver.get("https://www.facebook.com/whitehat/accounts/");
-		Thread.sleep(5);
+		//driver.get("https://www.facebook.com/whitehat/accounts/");
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(5000);
 		//inicia sesion con la cuenta principal
 		driver.findElement(gmailLocator).sendKeys(gmailaccount);		
 		driver.findElement(passwordLocator).sendKeys(newpassword);
 		driver.findElement(loginLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(10000);
 		//crea una cuenta de prueba en fecbook con el user principal
-		driver.findElement(By.xpath("(//span[contains(text(),'Crear cuenta nueva')])[1]")).click();	
-		Thread.sleep(5);
+		/*driver.findElement(By.xpath("(//span[contains(text(),'Crear cuenta nueva')])[1]")).click();	
+		Thread.sleep(10000);
 		String accountTest = driver.findElement(By.xpath("(//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh jq4qci2q a3bd9o3v knj5qynh oo9gr5id hzawbc8m'])[3]")).getText();
 		String accountTestPass = driver.findElement(By.xpath("(//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh jq4qci2q a3bd9o3v knj5qynh oo9gr5id hzawbc8m'])[4]")).getText();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("(//span[contains(text(),'Cerrar')])")).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		driver.findElement(menuLocator).click();
 		driver.findElement(logoutLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		
 		//inicia session con la cuenta de prueba 
 		driver.findElement(gmailLocator).sendKeys(accountTest);		
 		driver.findElement(passwordLocator).sendKeys(accountTestPass);
 		driver.findElement(By.xpath("(//button[@name='login'])")).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);*/
 				
 		
 	};	
@@ -91,11 +92,15 @@ public class FacebookTest {
 	
 		//crea la publicacion
 		driver.findElement(inicioLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("(//div[@aria-label='Ahora no'])[2]")).click();
+		Thread.sleep(2000);
 		driver.findElement(newpublicationLocator).click();
+		Thread.sleep(3000);
 		driver.findElement(publicationtextLocator).sendKeys("hola mundo,");
+		Thread.sleep(3000);
 		driver.findElement(publicarLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		
 		
 		
@@ -106,12 +111,15 @@ public class FacebookTest {
 	@Test(priority=2)
 	public void editPost() throws InterruptedException{	
 		//edita la publicacion
+		Thread.sleep(5000);
 		driver.findElement(opcionespublicacionLocator).click();
+		Thread.sleep(3000);
 		driver.findElement(editarpublicacionLocator).click();
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("(//div[@role='textbox'])[2]")).sendKeys(" fue un gusto");
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		driver.findElement(guardarpublicacionLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		
 		
 	};
@@ -120,29 +128,31 @@ public class FacebookTest {
 	public void deletePost() throws InterruptedException{
 	
 		driver.findElement(opcionespublicacionLocator).click();
+		Thread.sleep(3000);
 		driver.findElement(borrarpublicacionLocator).click();
+		Thread.sleep(3000);
 		driver.findElement(confirmareliminacionLocator).click();
-		Thread.sleep(5);
-		driver.findElement(menuLocator).click();
-		driver.findElement(logoutLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
+		
 		
 	};
 	
   
   @AfterSuite
   public void afterSuite() throws InterruptedException {
-	  	driver.get("https://www.facebook.com/whitehat/accounts/");
+	  	/*driver.get("https://www.facebook.com/whitehat/accounts/");
 		driver.findElement(gmailLocator).sendKeys(gmailaccount);		
 		driver.findElement(passwordLocator).sendKeys(newpassword);
 		driver.findElement(loginLocator).click();
-		Thread.sleep(5);
+		Thread.sleep(5000);
 		driver.findElement(seleccionarCuentaLocator).click();
 		driver.findElement(eliminarSeleccionLocator).click();
-		Thread.sleep(5);
-		driver.findElement(cerrarLocator).click();
+		Thread.sleep(5000); 
+		driver.findElement(cerrarLocator).click();*/
 		driver.findElement(menuLocator).click();
+		Thread.sleep(2000);
 		driver.findElement(logoutLocator).click();
+		Thread.sleep(5000);
 		driver.quit();
   }
   
